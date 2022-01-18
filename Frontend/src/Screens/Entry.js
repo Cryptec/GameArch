b2f2f07a6ce94052eb412c19bc646f3105390099
Cryptec <neupi92@web.de>
2022-01-18 11:55:01 +0100

    added Chart.js with examples on entry

--- Frontend/src/Screens/Entry.js
+++ Frontend/src/Screens/Entry.js
@@ -1,17 +1,62 @@
 import React from 'react'
+import { LineChart, BarChart, DoughnutChart, getData } from '../components/Chart/Chart'
 
+class Entry extends React.Component {
+  constructor(props) {
+    super(props);
 
-function Entry() {
+    this.state = {
+      data: getData()
+    };
+  }
+
+  componentDidMount() {
+    window.setInterval(() => {
+      this.setState({
+        data: getData()
+      })
+    }, 5000)
+  }
+
+  render() {
     return (
       <div>
     
         <div id="contentpage">
           <div className="overviewContainer">
-           <h3 style={{color: 'var(--text-primary)'}}>Home</h3>
+          <div className="main chart-wrapper">
+          <LineChart
+            data={this.state.data[0].data}
+            title={this.state.data[0].title}
+            color="#3E517A"
+          />
+        </div>
+        <div className="sub chart-wrapper">
+          <BarChart
+            data={this.state.data[1].data}
+            title={this.state.data[1].title}
+            color="#70CAD1"
+          />
+        </div>
+        <div className="sub chart-wrapper">
+          <BarChart
+            data={this.state.data[2].data}
+            title={this.state.data[2].title}
+            color="#B08EA2"
+          />
+        </div>
+        <div className="sub chart-wrapper">
+          <DoughnutChart
+            data={this.state.data[3].data}
+            title={this.state.data[3].title}
+            colors={['#a8e0ff', '#8ee3f5', '#70cad1', '#3e517a', '#b08ea2', '#BBB6DF']}
+          />
+        </div>
           </div>
         </div>
       </div>
     )
+  }
   }
 
 
---
+++ Frontend/src/components/Chart/Chart.js
@@ -0,0 +1,200 @@
+import React from 'react'
+import Chart from 'chart.js/auto'
+
+// Data generation
+function getRandomArray(numItems) {
+  // Create random array of objects
+  let names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let data = [];
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      label: names[i],
+      value: Math.round(20 + 80 * Math.random())
+    });
+  }
+  return data;
+}
+
+function getRandomDateArray(numItems) {
+  // Create random array of objects (with date)
+  let data = [];
+  let baseTime = new Date('2018-05-01T00:00:00').getTime();
+  let dayMs = 24 * 60 * 60;
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      time: 'Monday',
+      value: Math.round(20 + 80 * Math.random())
+    });
+  }
+  return data;
+}
+
+export function getData() {
+  let data = [];
+
+  data.push({
+    title: 'Money spent',
+    data: getRandomDateArray(20)
+  });
+
+  data.push({
+    title: 'Categories',
+    data: getRandomArray(20)
+  });
+
+  data.push({
+    title: 'Categories',
+    data: getRandomArray(10)
+  });
+
+  data.push({
+    title: 'Data 4',
+    data: getRandomArray(6)
+  });
+
+  return data;
+}
+
+
+// BarChart
+export class BarChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.myChart.data.labels = this.props.data.map(d => d.label);
+    this.myChart.data.datasets[0].data = this.props.data.map(d => d.value);
+    this.myChart.update();
+  }
+
+  componentDidMount() {
+    this.myChart = new Chart(this.canvasRef.current, {
+      type: 'bar',
+      options: {
+	      maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: 100
+              }
+            }
+          ]
+        }
+      },
+      data: {
+        labels: this.props.data.map(d => d.label),
+        datasets: [{
+          label: this.props.title,
+          data: this.props.data.map(d => d.value),
+          backgroundColor: this.props.color
+        }]
+      }
+    });
+  }
+
+  render() {
+    return (
+        <canvas ref={this.canvasRef} />
+    );
+  }
+}
+
+// LineChart
+export class LineChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.myChart.data.labels = this.props.data.map(d => d.time);
+    this.myChart.data.datasets[0].data = this.props.data.map(d => d.value);
+    this.myChart.update();
+  }
+
+  componentDidMount() {
+    this.myChart = new Chart(this.canvasRef.current, {
+      type: 'line',
+      options: {
+			  maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              type: 'time',
+              time: {
+                unit: 'week'
+              }
+            }
+          ],
+          yAxes: [
+            {
+              ticks: {
+                min: 0
+              }
+            }
+          ]
+        }
+      },
+      data: {
+        labels: this.props.data.map(d => d.time),
+        datasets: [{
+          label: this.props.title,
+          data: this.props.data.map(d => d.value),
+          fill: 'none',
+          backgroundColor: this.props.color,
+          pointRadius: 2,
+          borderColor: this.props.color,
+          borderWidth: 1,
+          lineTension: 0
+        }]
+      }
+    });
+  }
+
+  render() {
+    return <canvas ref={this.canvasRef} />;
+  }
+}
+
+
+// Doughnut
+export class DoughnutChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.myChart.data.labels = this.props.data.map(d => d.label);
+    this.myChart.data.datasets[0].data = this.props.data.map(d => d.value);
+    this.myChart.update();
+  }
+
+  componentDidMount() {
+    this.myChart = new Chart(this.canvasRef.current, {
+      type: 'doughnut',
+      options: {
+	      maintainAspectRatio: false
+      },
+      data: {
+        labels: this.props.data.map(d => d.label),
+        datasets: [{
+          data: this.props.data.map(d => d.value),
+          backgroundColor: this.props.colors
+        }]
+      }
+    });
+
+  }
+
+
+  render() {
+    return <canvas ref={this.canvasRef} />;
+  }
+}
+
+
