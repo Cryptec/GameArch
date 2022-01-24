52e253e955836d5e577475c762f61b81bc838a63
Cryptec <neupi92@web.de>
2022-01-24 20:42:05 +0100

    restored homescreen for dev

--- Frontend/src/Screens/Entry.js
+++ Frontend/src/Screens/Entry.js
@@ -1,65 +1,18 @@
 import React from 'react'
-import { LineChart, BarChart, DoughnutChart, getData } from '../components/Chart/Chart'
 
-import '../css/chart.css'
 
-class Entry extends React.Component {
-  constructor(props) {
-    super(props);
+function Entry() {
+  return (
+    <div>
 
-    this.state = {
-      data: getData()
-    };
-  }
-
-  componentDidMount() {
-    window.setInterval(() => {
-      this.setState({
-        data: getData()
-      })
-    }, 5000)
-  }
-
-  render() {
-    return (
-      <div>
-    
-        <div id="contentpage">
-          <div className="overviewContainer">
-          <div className="main chart-wrapper">
-          <LineChart
-            data={this.state.data[0].data}
-            title={this.state.data[0].title}
-            color="#3E517A"
-          />
-        </div>
-        <div className="sub chart-wrapper">
-          <BarChart
-            data={this.state.data[1].data}
-            title={this.state.data[1].title}
-            color="#70CAD1"
-          />
-        </div>
-        <div className="sub chart-wrapper">
-          <BarChart
-            data={this.state.data[2].data}
-            title={this.state.data[2].title}
-            color="#B08EA2"
-          />
-        </div>
-        <div className="sub chart-wrapper">
-          <DoughnutChart
-            data={this.state.data[3].data}
-            title={this.state.data[3].title}
-            colors={['#a8e0ff', '#8ee3f5', '#70cad1', '#3e517a', '#b08ea2', '#BBB6DF']}
-          />
-        </div>
-          </div>
+      <div id="contentpage">
+        <div className="overviewContainer">
+          <h3 style={{ color: 'var(--text-primary)' }}>Home</h3>
         </div>
       </div>
-    )
-  }
-  }
+    </div>
+  )
+}
 
 
 export default Entry
