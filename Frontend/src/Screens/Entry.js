64e3a54d59cfd62b83ff6bfcc200db97ed7f374d
Cryptec <neupi92@web.de>
2022-02-02 12:52:11 +0100

    add lastAdded field to entry

--- Frontend/src/Screens/Entry.js
+++ Frontend/src/Screens/Entry.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import LastAdded from './HomeSections/LastAdded'
 
 
 function Entry() {
   return (
-    <div>
 
       <div id="contentpage">
         <div className="overviewContainer">
           <h3 style={{ color: 'var(--text-primary)' }}>Home</h3>
         </div>
+        <LastAdded />
       </div>
-    </div>
+
   )
 }
 
---
+++ Frontend/src/Screens/HomeSections/LastAdded.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import ImagePlaceholder from '../../assets/imageplaceholder.png'
+import Rendercurrency from '../../utils/renderCurrency'
+
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
+
+class LastAdded extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      games: [],
+      isLoading: false,
+      isError: false,
+      ownagePreviewOk: <>&#10004;</>,
+      ownagePreviewFalse: <>&#x2715;</>,
+      filter: localStorage.getItem('filter'),
+      file: false
+    }
+}
+
+async componentDidMount() {
+  this.setState({ isLoading: true, file: ImagePlaceholder })
+  const response = await fetch(`${API_ENDPOINT}/api/gamedata/${this.state.filter}`, {credentials: 'include'})
+  if (response.ok) {
+    const games = await response.json()
+    this.setState({ games, isLoading: false })
+  } else {
+    this.setState({ isError: true, isLoading: false })
+  }
+}
+
+  render() {
+
+    return (
+      <div>
+
+        <div className="overviewContainer" style={{overflow: 'hidden'}}>
+        <h3>Last added:</h3>
+         <div style={{flexDirection: 'row', display: 'flex', flexDirection: 'row-reverse'}}>
+           {this.renderGames()}
+         </div>
+        </div>
+
+      </div>
+    )
+}
+
+
+renderGames = () => {
+
+  return this.state.games.map(game => {
+
+      const imageName = game.filename
+      const id = game.id
+      const title = game.title
+      const price = game.price
+      const purchasedate = game.purchasedate
+      const description = game.description
+      const region = game.region
+      const released = game.released
+      const ownage = game.ownage
+      const manual = game.manual
+      const box = game.box
+      const platform = game.platform
+      const wishlist = game.iswishlist
+      const stars = game.stars
+      const url = `${API_ENDPOINT}/uploads/${imageName}`
+     
+    return (
+         
+       <div key={game.id} >
+
+        <Link to={{ pathname: `/gamedetail/${title}`, 
+                    state: { titlename: title,
+                             description: description,
+                             price: price,
+                             purchasedate: purchasedate,
+                             id: id,
+                             filename: imageName,
+                             platform: platform,
+                             region: region,
+                             released: released,
+                             ownage: ownage,
+                             manual: manual,
+                             box: box,
+                             wishlist: wishlist,
+                             stars: stars }
+                  }} >
+        <div className="imageWrapper" style={{margin: '12px', border: 'var(--border)'}}>
+        
+            {imageName !== "null" ? <img src={`${url}`} alt="" className="imagePreview" />
+              : <img src={`${ImagePlaceholder}`} alt="" className="imagePreview" />}
+
+        </div>
+        </Link>
+
+        </div>
+      
+    )
+  })
+}
+}
+
+export default LastAdded
