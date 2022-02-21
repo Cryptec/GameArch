8c307004aca3792ed4d890e9364fced1663acdfe
Cryptec <neupi92@web.de>
2022-02-21 12:47:02 +0100

    clear unused vars

--- Frontend/src/Screens/Detailview.js
+++ Frontend/src/Screens/Detailview.js
@@ -184,16 +184,11 @@ class Detailview extends Component {
     var modal = document.getElementById("myModal");
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById("gameImg");
     var modalImg = document.getElementById("img01");
     const url = `${API_ENDPOINT}/uploads/${this.state.filename}`
 
       modal.style.display = "block";
       modalImg.src = `${url}`;
-    
-    
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
     
   }
 
@@ -230,7 +225,7 @@ class Detailview extends Component {
               </div>
               <div id="myModal" className="modal">
                 <span className="close" onClick={this.closeModal}>&times;</span>
-                <img className="modal-content" id="img01" />
+                <img className="modal-content" alt='modal-content' id="img01" />
               </div>
 
               <div style={{ flexDirection: 'column' }}>
--- Frontend/src/Screens/PublicScreens/PublicDetailview.js
+++ Frontend/src/Screens/PublicScreens/PublicDetailview.js
@@ -106,16 +106,11 @@ class PublicDetailview extends Component {
         var modal = document.getElementById("myModal");
     
         // Get the image and insert it inside the modal - use its "alt" text as a caption
-        var img = document.getElementById("gameImg");
         var modalImg = document.getElementById("img01");
         const url = `${API_ENDPOINT}/uploads/${this.state.filename}`
     
           modal.style.display = "block";
           modalImg.src = `${url}`;
-        
-        
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
         
       }
     
@@ -149,7 +144,7 @@ class PublicDetailview extends Component {
                                 </div>
                                 <div id="myModal" className="modal">
                                   <span className="close" onClick={this.closeModal}>&times;</span>
-                                  <img className="modal-content" id="img01" />
+                                  <img className="modal-content" alt='modal-content' id="img01" />
                                 </div>
 
                                 <div style={{ flexDirection: 'column' }}>
