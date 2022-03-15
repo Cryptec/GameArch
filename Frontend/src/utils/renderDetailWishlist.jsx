9a3061e6d7684c077273b3e4bf5a01ca69d16114
Cryptec <neupi92@web.de>
2022-03-15 22:28:01 +0100

    added sold icon to gridview, switched to jsx

--- Frontend/src/utils/renderDetailWishlist.jsx
+++ Frontend/src/utils/renderDetailWishlist.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ImagePlaceholder from '../assets/imageplaceholder.png'
-import Rendercurrency from '../utils/renderCurrency'
+import Rendercurrency from './renderCurrency'
 
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
 
@@ -14,17 +14,34 @@ class RenderDetailWishlist extends Component {
             isError: false,
             ownagePreviewOk: <>&#10004;</>,
             ownagePreviewFalse: <>&#x2715;</>,
+            ownagePreviewSold: <div style={{ marginLeft: '2px', marginRight: '2px' }}>&#83;</div>,
             filter: localStorage.getItem('filter'),
             file: false
         }
     }
 
     async componentDidMount() {
+        this.fetchMode()
         this.setState({ isLoading: true, file: ImagePlaceholder })
         const response = await fetch(`${API_ENDPOINT}/api/wishlist/${this.state.filter}`, { credentials: 'include' })
         if (response.ok) {
             const games = await response.json()
             this.setState({ games, isLoading: false })
+        } else {
+            this.setState({ isError: true, isLoading: false })
+        }
+    }
+
+    fetchMode = async () => {
+        const response = await fetch(`${API_ENDPOINT}/api/settingsdata`, {
+            credentials: 'include',
+        })
+        if (response.ok) {
+            const fetchdata = await response.json()
+            this.setState({ fetchdata })
+            this.state.fetchdata.map((data) => {
+                return this.setState({ resolution: data.resolution })
+            })
         } else {
             this.setState({ isError: true, isLoading: false })
         }
@@ -79,8 +96,35 @@ class RenderDetailWishlist extends Component {
                     <div className="gameTitle">{title}</div>
                     <div className="bottomSection">
                         <div className="gamePrice">{price}&nbsp;<Rendercurrency /></div>
-                        {game.ownage === "false" ? <div className="ownagePreviewFalse">{this.state.ownagePreviewFalse}</div> :
-                            <div className="ownagePreviewOk">{this.state.ownagePreviewOk}</div>}
+                        {game.saleprice === '' && this.state.resolution === 'enabled' ? (
+                            <div>
+                                {game.ownage === 'false' ? (
+                                    <div className='ownagePreviewFalse'>
+                                        {this.state.ownagePreviewFalse}
+                                    </div>
+                                ) : (
+                                    <div className='ownagePreviewOk'>
+                                        {this.state.ownagePreviewOk}
+                                    </div>
+                                )}
+                            </div>
+                        ) : this.state.resolution === 'disabled' ? (
+                            <div>
+                                {game.ownage === 'false' ? (
+                                    <div className='ownagePreviewFalse'>
+                                        {this.state.ownagePreviewFalse}
+                                    </div>
+                                ) : (
+                                    <div className='ownagePreviewOk'>
+                                        {this.state.ownagePreviewOk}
+                                    </div>
+                                )}
+                            </div>
+                        ) : (
+                            <div className='ownagePreviewSold'>
+                                {this.state.ownagePreviewSold}
+                            </div>
+                        )}
                     </div>
                 </div>
 
